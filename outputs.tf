ff8d9dfa28cfd8c2a5b8b0ec608c1dab854bb9b4
Evin Dunn <evindunn14@gmail.com>
2021-11-06 13:39:37 -0700

    Adds vm mac address as output

--- outputs.tf
+++ outputs.tf
@@ -10,7 +10,8 @@ output "vms" {
   value = [
     for node_idx in range(var.node_count) : {
         hostname  = "${var.hostname_prefix}${node_idx}.${var.network.dns_domain}", 
-        ip        = "${local.network.ip_prefix}.${var.network.ip_start + node_idx}"
+        ip        = "${local.network.ip_prefix}.${var.network.ip_start + node_idx}",
+        mac       = "${libvirt_domain.vms[node_idx].network_interface[0].mac}"
     }
   ]
 }
